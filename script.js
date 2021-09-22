2be3664b6aa5d21ee318df7ef2fab51c197e646c
Ajay Singh <daltavida@gmail.com>
2021-09-22 23:25:00 +1030

    Create function to generate usernames

--- script.js
+++ script.js
@@ -81,3 +81,16 @@ const displayMovements = function (movements) {
 };
 
 displayMovements(account1.movements);
+
+const createUsername = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(" ")
+      .map((name) => name[0])
+      .join("");
+  });
+};
+
+createUsername(accounts);
+console.log(accounts);
