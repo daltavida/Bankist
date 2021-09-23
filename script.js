1b0c60139196847a9b905011354b38b0a2985190
Ajay Singh <daltavida@gmail.com>
2021-09-23 12:26:00 +1030

    Display UI if user logged in

--- script.js
+++ script.js
@@ -130,5 +130,11 @@ btnLogin.addEventListener("click", function (e) {
   currentAccount = accounts.find(
     (acc) => acc.username === inputLoginUsername.value
   );
-  console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(" ")[0]
+    }`;
+    containerApp.style.opacity = 100;
+  }
 });
