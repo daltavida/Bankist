577c1ac616f4d3944fdc6b81c504437712837321
Ajay Singh <daltavida@gmail.com>
2021-09-26 15:29:00 +1030

    Clear input after transfer

--- script.js
+++ script.js
@@ -152,6 +152,7 @@ btnTransfer.addEventListener("click", function (e) {
   const receiverAcc = accounts.find(
     (acc) => acc.username === inputTransferTo.value
   );
+  inputTransferAmount.value = inputTransferTo.value = "";
   if (
     amount > 0 &&
     receiverAcc &&
