971fe7d74f4bc6a58d040cbcb550cf387884706a
Ajay Singh <daltavida@gmail.com>
2021-09-26 15:29:00 +1030

    Delete user account and log out

--- script.js
+++ script.js
@@ -167,5 +167,15 @@ btnTransfer.addEventListener("click", function (e) {
 
 btnClose.addEventListener("click", function (e) {
   e.preventDefault();
-  if (inputCloseUsername.value === currentAccount.username && Number(inputClosePin.value) === currentAccount.pin)
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+    accounts.splice(index, 1);
+    containerApp.style.opacity = 0;
+  }
+  inputClosePin.value = inputCloseUsername.value = "";
 });
