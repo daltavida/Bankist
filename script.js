2cbc09de61bc431f2f2eb7a14f4b94b4b04aac54
Ajay Singh <daltavida@gmail.com>
2021-09-27 16:30:00 +1030

    Add function to verify loans

--- script.js
+++ script.js
@@ -165,6 +165,18 @@ btnTransfer.addEventListener("click", function (e) {
   }
 });
 
+btnLoan.addEventListener("click", function (e) {
+  e.preventDefault;
+  const amount = Number(inputLoanAmount.value);
+  if (
+    amount > 0 &&
+    currentAccount.movements.some((mov) => mov >= amount * 0.1)
+  ) {
+    currentAccount.movements.push(amount);
+    updateUI();
+  }
+});
+
 btnClose.addEventListener("click", function (e) {
   e.preventDefault();
   if (
