8f58d84162881c65a3267c1462b908c42aaadb26
Ajay Singh <daltavida@gmail.com>
2021-09-28 17:31:00 +1030

    Add function to sort movements

--- script.js
+++ script.js
@@ -63,10 +63,12 @@ const inputClosePin = document.querySelector(".form__input--pin");
 
 // Function to display movements on the app
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = "";
 
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? "deposit" : "withdrawal";
     const html = `
       <div class="movements__row">
